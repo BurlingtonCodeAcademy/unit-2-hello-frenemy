9409159b086f4edcddff627d430dd9741b448f63
maryreagan <maryreagan630@yahoo.com>
2024-02-08 15:25:43 -0600

    added instructions and starter code

---
+++ index.js
@@ -0,0 +1,16 @@
+function respond(name) {
+    // enter your name through name parameter
+    if (name) {
+        /* check if their name is 'darth vader' */
+        /* tell them to go away */
+    } else {
+        /* greet them warmly by their name */
+    }
+}
+
+respond("darth vader");
+// should print 'Noooooo! That's impossible!'
+respond("lex luthor");
+// should print 'Be gone, you villian!!!'
+respond("luke skywalker");
+// should print 'Hello luke skywalker, you are a wonderful friend!'
